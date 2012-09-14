4f9f40eeef8ab7066be44b6bb6c8bcc9c7a2e23c
Lieven Benoot <lievenbenoot@gmail.com>
2012-09-14 14:15:47 +0200

    implemented filter

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,15 +1,15 @@
 
 $(function() {  
     if(typeof localStorage["filterShowBoys"]== 'undefined')
-        localStorage["filterShowBoys"]=true;
+        localStorage["filterShowBoys"]='true';
     if(typeof localStorage["filterShowGirls"]== 'undefined')
-        localStorage["filterShowGirls"]=true;
+        localStorage["filterShowGirls"]='true';
     if(typeof localStorage["popular"]== 'undefined')
-        localStorage["popular"]=true;
+        localStorage["popular"]='true';
     if(typeof localStorage["original"]== 'undefined')
-        localStorage["original"]=true;
+        localStorage["original"]='true';
     if(typeof localStorage["common"]== 'undefined')
-        localStorage["common"]=true;
+        localStorage["common"]='true';
         
     $('div#fireFilterSection').click(function(event) {
         $('div#filter').slideToggle('slow', function() {
@@ -43,6 +43,41 @@ $(function() {
     }); 
 });
 
-function filterUpdated(){
+function filterUpdated(){    
+    var names=JSON.parse(localStorage['data']);
+    //total names=names[names.length-1];
+    var counter=0;
+    var filteredGender=new Array();
+    var showboys=localStorage["filterShowBoys"]=='true';
+    var showgirls=localStorage["filterShowGirls"]=='true';
+    $.each(names,function(id,name){
+        if(showboys && name.gender=='boy')
+            filteredGender.push(name);
+        if(showgirls && name.gender=='girl')
+            filteredGender.push(name);                
+    });     
+    var topOneThirth=Math.round(filteredGender.length/3);
+    var topTwoTirth=Math.round(topOneThirth*2);   
+    var counter=0;
+    var filteredNames=new Array();
+    var showPopular=localStorage["popular"]=='true';
+    var showCommon=localStorage["common"]=='true';
+    var showOriginal=localStorage["original"]=='true';
+    $.each(filteredGender,function(id,name){
+        if(counter<=topOneThirth ) {
+            if(showPopular)
+                filteredNames.push(name);
+        }
+        else if(counter<=topTwoTirth ){
+            if(showCommon)
+                filteredNames.push(name);
+        }
+        else if(showOriginal){         
+            filteredNames.push(name);
+        }        
+        counter++;
+    })
+    localStorage['filteredData'] =JSON.stringify(filteredNames);
+    console.log(JSON.parse(localStorage['filteredData']));
     
 }
