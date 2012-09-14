d26e0c43ca466cec91e2c8f4b1cfeffc43eaf537
SamVerschueren <sam.verschueren@gmail.com>
2012-09-14 15:36:50 +0200

    added images and style

--- scripts/main.js
+++ scripts/main.js
@@ -19,9 +19,13 @@ Array.prototype.remove = function(name) {
 }
 
 $(function() {   
-    /*localStorage['favorites'] = JSON.stringify(new Array("Jeroen", "Lieven"));
+    localStorage.clear();    
+    localStorage['favorites'] = JSON.stringify(new Array("Jeroen", "Lieven"));
     localStorage['recycleBin'] = JSON.stringify(new Array("Test", "Test 2"));
-    localStorage['accounts'] = JSON.stringify(new Array("Account 1", "Account 2"));*/  
+    /*localStorage['accounts'] = JSON.stringify(new Array("Account 1", "Account 2"));*/  
+    
+    /*var name = searchByName('Sam');
+    console.log(name);*/
     
     if(typeof localStorage['selectedAccount'] == 'undefined') {
         localStorage['selectedAccount'] = 'Account 1';
@@ -84,6 +88,16 @@ function loadFilteredJson() {
     else {
         window.location.href = 'home.html';
     }
+}
+
+function searchByName(name) {
+    var names=JSON.parse(localStorage['data']);
+    
+    $.each(names, function(key, val) {
+        if(val.name == name) {
+            return val;
+        }    
+    });
 }
 
 /**
